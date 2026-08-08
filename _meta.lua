a46dfe53c1fd87cb6e5de6b9a151aacaee0fa90d
qiuyukang <b612n@qq.com>
2026-08-08 14:36:32 +0800

    chore: release v0.5.2

--- _meta.lua
+++ _meta.lua
@@ -8,5 +8,5 @@ return {
     name = "one",
     fullname = _("ONE · 一个"),
     description = _([[Read the daily image, article and question from ONE (wufazhuce.com) offline in KOReader as a per-issue EPUB.]]),
-    version = "0.5.1",
+    version = "0.5.2",
 }
